e242710266a71125cd73fb88d6ca9178904d1aa7
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-10-30 22:06:29 +0000

    getArea doesn't work

--- Triangle.java
+++ Triangle.java
@@ -60,6 +60,43 @@ public String classify()
               else return "scalene";}
 
 public String toString() 
+{ return "v1(" + v1.getX() + ", " + v1.getY() 
+     + ") v2(" + v2.getX() + ", " + v2.getY() 
+     + ") v3(" + v3.getX() + ", " + v3.getY() + ")";}
+
+public void setVertex(int index, Point newP)
+{if (index == 0)
+{v1 = newP;}
+if (index == 1)
+{v2 = newP;}
+v3 = newP;}}
+
+                                                       
+                                                       
+public double getArea() {
+                          double semi = getPerimeter() / 2.0;
+                          return (Math.sqrt(semi - distance(v1.x,v1.y,v2.x,v2.y)) *
+                                        (semi - distance(v2.x,v2.y,v3.x,v3.y)) *
+                                        (semi - distance(v3.x,v3.y,v1.x,v1.y)));
+}
+
+              
+public String classify() 
+{          if (distance (v1.x,v1.y,v2.x,v2.y) ==
+              distance ( v2.x,v2.y,v3.x,v3.y) && 
+              distance ( v2.x,v2.y,v3.x,v3.y) ==
+              distance ( v3.x,v3.y,v1.x,v1.y))
+              return "equilateral";
+          if (distance ( v1.x,v1.y,v2.x,v2.y) ==
+              distance ( v2.x,v2.y,v3.x,v3.y) ||
+              distance ( v2.x,v2.y,v3.x,v3.y) ==
+              distance ( v3.x,v3.y,v1.x,v1.y) ||
+              distance ( v3.x,v3.y,v1.x,v1.y) ==
+              distance ( v1.x,v1.y,v2.x,v2.y))
+              return "isosceles";
+              else return "scalene";}
+
+public String toString() 
 { return "v1(" + v1 + ") " + "v2(" + v2 + ") " + "v3(" + ") "; }
 
 public void setVertex(int index, Point newP)
