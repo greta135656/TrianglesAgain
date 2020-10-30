220ad26cf2f590ff34153f862e266c4c79005654
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-10-30 20:54:13 +0000

    the code is wrong

---
+++ Triangle.java
@@ -0,0 +1,70 @@
+public class Triangle {
+ public Point v1, v2, v3;
+
+ // 2 constructors 
+public Triangle(Point a, Point b, Point c) 
+  {
+    v1 = a;
+    v2 = b;
+    v3 = c;
+      
+  }
+
+public Triangle(double x1, double y1,double x2, double y2,double x3, double y3)
+{
+ v1 = new Point (x1, y1);
+ v2 = new Point (x2, y2);
+ v3 = new Point (x3, y3);
+}
+
+// methods
+
+
+public static double distance(double x1, double y1,
+              double x2, double y2) {
+    
+  return Math.sqrt ((x2 - x1) * (x2 - x1) + (y2-y1) * (y2-y1));}
+
+
+
+public double getPerimeter() {
+return (distance (v1.x,v1.y,v2.x,v2.y) +
+        distance (v2.x,v2.y,v3.x,v3.y) +
+        distance (v3.x,v3.y,v1.x,v1.y));
+  }
+
+
+                                                       
+                                                       
+public double getArea() {
+                          double semi = getPerimeter() / 2.0;
+                          return (Math.sqrt(semi - distance(v1.x,v1.y,v2.x,v2.y)) *
+                                        (semi - distance(v2.x,v2.y,v3.x,v3.y)) *
+                                        (semi - distance(v3.x,v3.y,v1.x,v1.y)));
+}
+
+              
+public String classify() 
+{          if (distance (v1.x,v1.y,v2.x,v2.y) ==
+              distance ( v2.x,v2.y,v3.x,v3.y) && 
+              distance ( v2.x,v2.y,v3.x,v3.y) ==
+              distance ( v3.x,v3.y,v1.x,v1.y))
+              return "equilateral";
+          if (distance ( v1.x,v1.y,v2.x,v2.y) ==
+              distance ( v2.x,v2.y,v3.x,v3.y) ||
+              distance ( v2.x,v2.y,v3.x,v3.y) ==
+              distance ( v3.x,v3.y,v1.x,v1.y) ||
+              distance ( v3.x,v3.y,v1.x,v1.y) ==
+              distance ( v1.x,v1.y,v2.x,v2.y))
+              return "isosceles";
+              else return "scalene";}
+
+public String toString() 
+{ return "v1(" + v1 + ") " + "v2(" + v2 + ") " + "v3(" + ") "; }
+
+public void setVertex(int index, Point newP)
+{if (index == 0)
+{v1 = newP;}
+if (index == 1)
+{v2 = newP;}
+v3 = newP;}}
